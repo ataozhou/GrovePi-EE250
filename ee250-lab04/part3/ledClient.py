03f25ffbea35383692cbb2a928256f559a2cd78c
Andrew Zhou <andrewzh@usc.edu>
2018-02-27 16:22:41 -0800

    added code

--- ee250-lab04/part3/ledClient.py
+++ ee250-lab04/part3/ledClient.py
@@ -1,6 +1,32 @@
 import sys
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `from grovepi import *`
-sys.path.append('../../Software/Python/')
+#sys.path.append('../../Software/Python/')
 
-from grovepi import *
+#from grovepi import *
+
+
+import socket
+
+def Main():
+    """127.0.0.1 is the loopback address. Any packets sent to this address will
+    essentially loop right back to your machine and look for any process 
+    listening in on the port specified."""
+    host = '192.168.1.144'
+    port = 8000
+
+    s = socket.socket() #by default, the socket constructor creates an TCP/IPv4 socket
+    s.connect((host,port))
+
+    message = input("-> ")
+    while True:
+        s.send(message.encode('utf-8')) 
+        #1024 is the receive buffer size. It's enough for us, and it's a nice number. 
+        message = input("-> ")
+    s.close()
+
+"""This if-statement checks if you are running this python file directly. That 
+is, if you run `python3 tcpClient.py` in terminal, this if-statement will be 
+true"""
+if __name__ == '__main__':
+    Main()
--- ee250-lab04/part3/ultrasonicServer.py
+++ ee250-lab04/part3/ultrasonicServer.py
@@ -1,8 +1,30 @@
 import sys
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `from grovepi import *`
-sys.path.append('../../Software/Python/')
+# sys.path.append('../../Software/Python/')
 
-from grovepi import *
+# from grovepi import *
+
+import socket
+
+def Process1():
+	# Change the host and port as needed. For ports, use a number in the 9000 
+	# range. 
+	host = '10.0.2.15'
+	port = 9000
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((host,port))
+
+	print("Process Ultrasonic Server Started")
+	while True:
+		data, addr = s.recvfrom(1024)
+		data = data.decode('utf-8')
+		print("Distance: " + data)
+	c.close()
+
+if __name__ == '__main__':
+	Process1()
+
 
 #use UDP
