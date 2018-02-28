34b38a1462dc5e90ff0c97e13af0d1bfc3e3f4f4
harrisonforch <forch@example.edu>
2018-02-28 00:42:32 +0000

    added code to led Client and ultrasonic Server

--- ee250-lab04/part3/ledClient.py
+++ ee250-lab04/part3/ledClient.py
@@ -4,3 +4,38 @@ import sys
 sys.path.append('../../Software/Python/')
 
 from grovepi import *
+
+import socket
+
+def Main():
+    """127.0.0.1 is the loopback address. Any packets sent to this address will
+    essentially loop right back to your machine and look for any process 
+    listening in on the port specified."""
+	host = '192.168.1.144'
+	port = 8000
+
+	s = socket.socket() #by default, the socket constructor creates an TCP/IPv4 socket
+	s.connect((host,port))
+
+	led = 2
+	pinMode(led,"OUTPUT")
+	message = input("-> ")
+
+	while message != 'q':
+		s.send(message.encode('utf-8'))
+		#1024 is the receive buffer size. It's enough for us, and it's a nice number. 
+		data = s.recv(1024).decode('utf-8')
+		if data == "LED_ON":
+			digitalWrite(led,1)
+		else if data == "LED_OFF":
+			digitalWrite(led,0)
+		else:
+			print("Please input LED_ON, LED_OFF, or q to quit")
+		message = input("-> ")
+	s.close()
+
+"""This if-statement checks if you are running this python file directly. That 
+is, if you run `python3 tcpClient.py` in terminal, this if-statement will be 
+true"""
+if __name__ == '__main__':
+	Main()
--- ee250-lab04/part3/ultrasonicClient.py
+++ ee250-lab04/part3/ultrasonicClient.py
@@ -3,6 +3,32 @@ import sys
 # we are successfully `from grovepi import *`
 sys.path.append('../../Software/Python/')
 
-from grovepi import *
+import grovepi
+
+import socket
+
+def Main():
+	# Change the host and port as needed. For ports, use a number in the 9000
+	# range.
+	host = '192.168.1.144'
+	port = 8000
+
+	server_addr = '192.168.1.185'
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((host,port))
+
+	# UDP is connectionless, so a client does not formally connect to a server
+	# before sending a message.
+	dst_port = input("destination port-> ")
+	while True:
+		#tuples are immutable so we need to overwrite the last tuple
+		server = (server_addr, int(dst_port))
+		message = (grovepi.ultrasonicRead(2))
+		s.sendto((str(message)).encode('utf-8'), server)
+	s.close()
+
+if __name__ == '__main__':
+	Main()
 
 #use UDP
